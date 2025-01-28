5cec15ffd17bc567bf2adb8b29ddaa4dec556eda
Arkadiy Ivanov <digitmir@gmail.com>
2025-01-29 00:51:56 +0300

    add settings to connect to db

---
+++ shop_app/models/settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    def DATABASE_URL_asyncpg(self) -> str:
+        """Формирует строку подключения для asyncpg."""
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file="../../.env")
+
+settings = Settings()
+database_url = settings.DATABASE_URL_asyncpg()
+
+# ?charset=utf8mb4_unicode_ci
